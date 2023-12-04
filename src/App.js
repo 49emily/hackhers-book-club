2e134d85e9550e64dcf0bf129689ab627a81d4f0
Emily Zhang <emily49@stanford.edu>
2023-12-03 18:12:43 -0800

    pr

--- src/App.js
+++ src/App.js
@@ -20,19 +20,32 @@ function App() {
     },
   });
 
-  const introText = `Welcome Emily and Sharon to today’s session. You’re about to do session 2 of your “Search
-  Inside Yourself journey.” This will hopefully be an enjoyable and bonding experience while
-  also being an introduction to mindfulness and emotional intelligence. Today we will practice
+  const introText = `Welcome Emily and Sharon to today’s session. Today we will practice
   “mindful listening exercise".`;
 
-  const start = () => {
-    speak({ text: introText, voice: voices[0] });
-  };
+  const activeListeningText = `In this exercise, we will practice listening in a way that is different from how we usually listen. We will do this in pairs - each person taking turns to be the speaker and the listener.`;
 
+  const text3 = "Emily first you are the speaker. Sharon, you are the listener.";
+
+  const text4 =
+    "Emily, this will be a monologue. You get to speak uninterrupted for 3 minutes on a prompt. If you run out of things to say, that is fine; you can just sit in silence and whenever you have something to say, you may continue speaking again.";
+
+  const text5 =
+    "Your job is to listen. When you listen, give your full attention to the speaker. You may not ask questions during these 3 minutes. You may acknowledge with facial expressions, by nodding your head, or by saying, “I see,” or “I understand.” You may not speak except to acknowledge";
+  const audios = [introText, activeListeningText, text3, text4, text5];
+
+  // const start = () => {
+  //   speak({ text: introText, voice: voices[0] });
+  // };
+
+  // const speak2 = () => {
+  //   speak({ text: activeListeningText, voice: voices[0] });
+  // };
+
+  const [index, setIndex] = useState(0);
   return (
     <div className="App">
       <div className="main-container">
-        <Button onClick={start}>Start</Button>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
@@ -44,11 +57,34 @@ function App() {
           }}
           className="ai-text"
         >
-          <TypeAnimation sequence={[introText]} />
+          <TypeAnimation
+            sequence={[
+              introText,
+              2000,
+              activeListeningText,
+              1000,
+              text3,
+              1000,
+              text4,
+              4000,
+              text5,
+              4000,
+            ]}
+            omitDeletionAnimation="true"
+          />
         </motion.div>
-        <p>{value}</p>
-        <Button onClick={listen}>{listening ? "Listening" : "Listen"} </Button>
+
+        {/* <Button onClick={listen}>{listening ? "Listening" : "Listen"} </Button> */}
         {/* <p>{response}</p> */}
+        <Button
+          className="button"
+          onClick={() => {
+            speak({ text: audios[index], voice: voices[1] });
+            setIndex(index + 1);
+          }}
+        >
+          Start
+        </Button>
       </div>
     </div>
   );

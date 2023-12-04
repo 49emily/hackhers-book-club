628bf21891500ee5d2e943ce0efa43c25e8beb62
Emily Zhang <emily49@stanford.edu>
2023-12-03 17:10:13 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useSpeechRecognition, useSpeechSynthesis } from "react-speech-kit";
+import { Button } from "antd";
+import "./App.css";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+
+// import * as ai from "@replit/ai";
 
 function App() {
+  const [value, setValue] = useState("");
+  const [response, setResponse] = useState("");
+  const { speak, voices } = useSpeechSynthesis();
+
+  // let response = "";
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: (result) => {
+      setResponse((oldResponse) => oldResponse + result);
+      setValue(result);
+    },
+  });
+
+  const introText = `Welcome Emily and Sharon to today’s session. You’re about to do session 2 of your “Search
+  Inside Yourself journey.” This will hopefully be an enjoyable and bonding experience while
+  also being an introduction to mindfulness and emotional intelligence. Today we will practice
+  “mindful listening exercise".`;
+
+  const start = () => {
+    speak({ text: introText, voice: voices[0] });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="main-container">
+        <Button onClick={start}>Start</Button>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          //  animate={{ y: [100, 0] }}
+          transition={{
+            duration: 0.8,
+            delay: 0.5,
+            ease: [0, 0.71, 0.2, 1.01],
+          }}
+          className="ai-text"
         >
-          Learn React
-        </a>
-      </header>
+          <TypeAnimation sequence={[introText]} />
+        </motion.div>
+        <p>{value}</p>
+        <Button onClick={listen}>{listening ? "Listening" : "Listen"} </Button>
+        {/* <p>{response}</p> */}
+      </div>
     </div>
   );
 }
